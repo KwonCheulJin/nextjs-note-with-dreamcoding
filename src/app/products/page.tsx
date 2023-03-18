b711189cbe592aae22e40ec4d326883497d66f84
KwonCheulJin <chkftm12@gmail.com>
2023-03-18 17:33:38 +0900

    SSG, ISR, and SSR using fetch

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,18 @@
 import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
+import styles from './page.module.css';
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
+  const res = await fetch('https://meowfacts.herokuapp.com', {
+    // next: { revalidate: 3 },
+    // cache: 'no-store',
+  });
+  const data = await res.json();
+  const factText = data.data[0];
   const productList = products.map(({ id, name }) => (
     <li key={id}>
       <Link href={`/products/${id}`}>{name}</Link>
@@ -15,6 +22,7 @@ export default async function ProductsPage() {
     <>
       <h1>제품 상세 페이지!</h1>
       <ul>{productList}</ul>
+      <article className={styles.article}>{factText}</article>
     </>
   );
 }

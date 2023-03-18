1136196943ed43bfba8d71e6bdd9b20de5dece33
KwonCheulJin <chkftm12@gmail.com>
2023-03-18 15:20:55 +0900

    next default server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import os from 'os';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('🚀 ~ file: page.tsx:11 ~ Home ~ 홈페이지다:');
+  console.log('🚀 ~ file: page.tsx:5 ~ os:', os.hostname());
   return <h1>홈페이지다!</h1>;
 }
